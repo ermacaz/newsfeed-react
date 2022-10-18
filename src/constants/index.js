de9efe5e6134e0886c638581588ab181ec62fc42
ermacaz <ermacaz@protonmail.com>
2022-10-17 20:55:14 -0700

    Remove placeholder favicon

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,7 @@
 // src/constants/index.js
 export const API_ROOT = 'https://newsfeedapi.ermacaz.com';
 // export const API_ROOT = 'http://localhost:3001';
-export const API_WS_ROOT = 'wss://news/feedapi.ermacaz.com/cable';
+export const API_WS_ROOT = 'wss://newsfeedapi.ermacaz.com/cable';
 // export const API_WS_ROOT = 'wss://localhost:3001';
 export const HEADERS = {
   'Content-Type': 'application/json',
